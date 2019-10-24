1043b68ca0a21ed1480729e58e7e27a27900f0dd
Zuo-Cheng <1807002272@qq.com>
2019-10-25 00:30:11 +0800

    第三天知识

---
+++ app.js
@@ -0,0 +1,37 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log("我是小程序初始化后的函数");
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log(options)
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log('小程序关闭了')
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+
+  //在app.js文件中定义的全局数据，在每一个项目都能使用这些数据
+  globaData:{
+    name:'zuocheng',
+    age:18,
+    gender:'男'
+  }
+})
