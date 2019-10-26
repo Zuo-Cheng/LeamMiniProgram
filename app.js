f9fa5f1c625720e2deea377f51567f231ec5057c
Zuo-Cheng <1807002272@qq.com>
2019-10-26 08:24:54 +0800

    第四天内容

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
+  onLaunch: function (options) {
     console.log("我是小程序初始化后的函数");
+    console.log(options)
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
+    console.log("我是小程序的启动生命周期函数")
     console.log(options)
   },
 
--- pages/about/about.js
+++ pages/about/about.js
@@ -6,11 +6,24 @@ console.log(app.globaData.age)
 
 Page({
   data:{
+    msg:'我是张三',
+    age:18
 
   },
   userInfo:function(event){
     
     console.log(event)
+    wx.getUserInfo({
+      success:function(res){
+        console.log(res);
+      }
+    })
+  },
+  getUserInf2:function(e){
+    console.log(e);
+  },
+  onLoad:function(){
+    console.log('hahahah')
   }
 })
 
